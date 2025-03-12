7eeb472ea671f38c22c3e4eae0383c70f4c0f3d7
Cndynajae <csagitasari@gmail.com>
2025-03-12 16:29:53 +0700

    membuat board, profile, datanya belum nampil

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,25 +1,33 @@
-import 'package:get/get.dart';
 import 'dart:async';
+
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:intra_sub_mobile/app/modules/dashboard/views/dashboard_view.dart';
 import 'package:intra_sub_mobile/app/modules/login/views/login_view.dart';
 
 class HomeController extends GetxController {
+  //TODO: Implement HomeController
+
   late Timer _pindah;
+  final authToken = GetStorage();
 
   @override
   void onInit() {
+    _pindah = Timer.periodic(
+      const Duration(seconds: 4),
+      (timer) => authToken.read('token') == null
+          ? Get.off(
+              () => const LoginView(),
+              transition: Transition.leftToRight,
+            )
+          : Get.off(() => const DashboardView()),
+    );
     super.onInit();
   }
 
   @override
   void onReady() {
-    _pindah = Timer.periodic(
-      const Duration(seconds: 4),
-      (timer) => Get.off(
-        () => const LoginView(),
-        transition: Transition.leftToRight,
-      ),
-    );
-    super.onInit();
+    super.onReady();
   }
 
   @override
